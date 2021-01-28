9f5388f230b6dcb92dcdb35ea2c6b0a6ca1bdfa7
Onabanjo Titus <74909457+OnabanjoTitus@users.noreply.github.com>
2021-01-28 10:39:38 +0100

    commit

---
+++ PythonListManipulations.py
@@ -0,0 +1,33 @@
+firstList = {1, 2, 3, 4, 5}
+print(firstList)
+seconder = {"ace", "ball", "try"}
+firstList.append(22)
+print(firstList)
+
+firstList.insert(0, 99)
+print(firstList)
+
+firstList[0] += 1
+print(firstList)
+
+firstList += [66]
+print(firstList)
+
+firstList.remove(3)
+print(firstList)
+
+firstList.pop()
+print(firstList)
+
+seconder.pop()
+print(seconder)
+
+secondList = ["Tito", "Titus"]
+print(secondList)
+
+thirdList = "-".join(secondList)
+print(thirdList)
+
+thirdList = "*".join(secondList)
+print(thirdList)
+
