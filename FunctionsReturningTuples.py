23b820efd07afc4352a9a36d0692156d82877814
Onabanjo Titus <74909457+OnabanjoTitus@users.noreply.github.com>
2021-01-29 08:50:53 +0100

    commit

---
+++ FunctionsReturningTuples.py
@@ -0,0 +1,10 @@
+first, second, third = 'Doug', 22, 1984
+
+
+def functionsReturningTuple(a, b, c):
+    x = (c, a, b)
+    return x
+
+
+print(functionsReturningTuple(first, second, third))
+print(functionsReturningTuple(first, second, third))
